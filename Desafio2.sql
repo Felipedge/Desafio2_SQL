745da783c0b22274f8b57ad23dbb47548fdda36f
Felipe P <fpalmasaldivia@gmail.com>
2022-11-12 21:02:48 -0300

    tercera parte

--- Desafio2.sql
+++ Desafio2.sql
@@ -19,3 +19,11 @@ Respuesta: SELECT DISTINCT(fuente), SUM(cantidad) FROM INSCRITOS GROUP BY fuente
 inscribieron en ese día? */
 Respuesta: SELECT DISTINCT(fecha), SUM(cantidad) FROM INSCRITOS GROUP by fecha ORDER BY SUM(cantidad);
 
+/* ¿Qué días se inscribieron la mayor cantidad de personas utilizando el blog y cuántas personas fueron? */
+Respuesta: SELECT * FROM INSCRITOS WHERE cantidad = (SELECT MAX(cantidad) FROM INSCRITOS WHERE fuente = 'Blog');
+
+/* ¿Cuántas personas en promedio se inscriben en un día? */
+Respuesta: SELECT AVG(total) FROM (SELECT fecha, SUM(cantidad) as total FROM INSCRITOS GROUP by fecha);
+
+/* ¿Qué días se inscribieron más de 50 personas? */
+SELECT fecha, SUM(cantidad) FROM INSCRITOS GROUP by fecha HAVING SUM(cantidad) > 50;
